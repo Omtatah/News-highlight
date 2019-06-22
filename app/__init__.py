08c57c5bbd27347031c896d12aa59e3dbdccd0ab
Omtatah <murtallahomtatah@gmail.com>
2019-06-22 15:33:37 +0300

    define the app function that is to be returned upon all the code being read by the server

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-rom flask import Flask
+from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 
@@ -6,5 +6,20 @@ bootstrap = Bootstrap()
 
 def create_app(config_name):
 	app = Flask(__name__)
- 
- return app
+
+	#create the app configurations
+	app.config.from_object(config_options[config_name])
+
+	# Initializing flask extensions
+	bootstrap.init_app(app)
+
+
+	#registering the blueprint
+	from .main import main as main_blueprint
+	app.register_blueprint(main_blueprint)
+
+	#setting config
+	from .requests import configure_request
+	configure_request(app)
+
+	return app
