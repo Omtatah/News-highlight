fccd9155a3e94117f9646888f849ff37fbbf3a00
Omtatah <murtallahomtatah@gmail.com>
2019-06-22 15:39:18 +0300

    define the get_sources class(more details on the class in the docstring) and the configure_request class

--- app/requests.py
+++ app/requests.py
@@ -1,3 +1,27 @@
 import urllib.request,json
 from .models import Sources,Articles
 from datetime import datetime
+
+def configure_request(app):
+	global api_key,base_url,articles_url
+	api_key = app.config['NEWS_API_KEY']
+	base_url = app.config['NEWS_SOURCES_BASE_URL']
+	articles_url = app.config['ARTICLES_BASE_URL']
+
+def get_sources(category):
+	'''
+	Function that gets the json response to our url request
+	'''
+	get_sources_url = base_url.format(category,api_key)
+
+	with urllib.request.urlopen(get_sources_url) as url:
+		get_sources_data = url.read()
+		get_sources_response = json.loads(get_sources_data)
+
+		sources_results = None
+
+		if get_sources_response['sources']:
+			sources_results_list = get_sources_response['sources']
+			sources_results = process_sources(sources_results_list)
+
+	return sources_results
